6d4b26d3002cc99d971b46518f7b58dec41c3043
jvillarreal <jvillarreal@lamisionsuper.com>
2020-10-28 22:40:50 +0000

    update 2010281

--- mAgendaPersonal/conexion_calendario.php
+++ mAgendaPersonal/conexion_calendario.php
@@ -1,6 +1,6 @@
 <?php
 	 try {
-	 	$bdd = new PDO('mysql:host=200.1.1.178;dbname=sysadmision2', 'root', 'Xoops1991');
+	 	$bdd = new PDO('mysql:host=200.1.1.178;dbname=sysadmision2', 'jvillarreal', 'Xoops1991');
 	 } catch(Exception $e) {
 	 	exit('Impossible de se connecter à la base de données.');
 	 }
