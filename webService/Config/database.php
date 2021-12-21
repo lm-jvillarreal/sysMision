1cd2fa0ef8c9ea404aea6980bd7bb3b0957d40ae
SebastianVillarreal <sebas_villalva13@hotmail.com>
2021-12-20 18:40:56 -0600

    Update database.php

--- webService/Config/database.php
+++ webService/Config/database.php
@@ -11,9 +11,17 @@ class Database{
     // get the database connection
     public function getConnection(){
 
+        /* Connect to a MySQL database using driver invocation */
+            $dsn_p = 'mysql:dbname=sysadmision2;host=74.208.211.84';
+            $user_p = 'mision';
+            $password_p = 'ABC1238f47';
+
+            $dbh = new PDO($dsn, $user, $password);
+
         $this->conn = null;
         try{
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn = new PDO($dsn_p, $user_p, $password_p);
+            //$this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
             //$this->conn->exec("set names utf8");
         }catch(PDOException $exception){
             echo "Connection error: " . $exception->getMessage();
