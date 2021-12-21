49173e466ec820255968f3689ef71d9990b279fe
SebastianVillarreal <sebas_villalva13@hotmail.com>
2021-12-20 18:49:47 -0600

    Update database.php

--- webService/Config/database.php
+++ webService/Config/database.php
@@ -2,11 +2,11 @@
 class Database{
 
     // specify your own database credentials
-    private $host = "74.208.211.84";
-    private $db_name = "sysadmision2";
-    private $username = "mision";
-    private $password = "ABC1238f47";
-    public $conn;
+    // private $host = "74.208.211.84";
+    // private $db_name = "sysadmision2";
+    // private $username = "mision";
+    // private $password = "ABC1238f47";
+    // public $conn;
 
     // get the database connection
     // public function getConnection(){
