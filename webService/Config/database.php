7bea2728db0c67dd7e27e2d94fb78c9934414b2a
SebastianVillarreal <sebas_villalva13@hotmail.com>
2021-12-20 18:49:15 -0600

    Update database.php

--- webService/Config/database.php
+++ webService/Config/database.php
@@ -9,27 +9,27 @@ class Database{
     public $conn;
 
     // get the database connection
-    public function getConnection(){
+    // public function getConnection(){
 
-        /* Connect to a MySQL database using driver invocation */
-        //$conexion=mysqli_connect($db_host,$db_user,$db_pass,$db_name);
+    //     /* Connect to a MySQL database using driver invocation */
+    //     //$conexion=mysqli_connect($db_host,$db_user,$db_pass,$db_name);
 
 
-        $this->conn = null;
-        $this->conn = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
-        if(mysqli_connect_errno()){
-         printf(mysqli_connect_error());
-        }
-        mysqli_set_charset($this->con, 'utf8');
-        // try{
+    //     $this->conn = null;
+    //     $this->conn = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
+    //     if(mysqli_connect_errno()){
+    //      printf(mysqli_connect_error());
+    //     }
+    //     mysqli_set_charset($this->con, 'utf8');
+    //     // try{
             
-        //     //$this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-        //     //$this->conn->exec("set names utf8");
-        // }catch(PDOException $exception){
-        //     echo "Connection error: " . $exception->getMessage();
-        // }
+    //     //     //$this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+    //     //     //$this->conn->exec("set names utf8");
+    //     // }catch(PDOException $exception){
+    //     //     echo "Connection error: " . $exception->getMessage();
+    //     // }
 
-        return $this->conn;
-    }
+    //     return $this->conn;
+    // }
 }
 ?>
